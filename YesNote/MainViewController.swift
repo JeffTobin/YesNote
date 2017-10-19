2c7f1ef46e344ef681128191c93a450143589d1e
JeffTobin <jeff@tobin.io>
2017-10-19 13:26:40 -0400

    Added dynamic table view for drone volume section

--- YesNote/MainViewController.swift
+++ YesNote/MainViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, UITableViewDataSource {
+    
+//----------------------------------------------------------------------------------------------------------
+// Drone Volume controlls
+//----------------------------------------------------------------------------------------------------------
+    @IBOutlet weak var DroneTableView: UITableView!
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = DroneTableView.dequeueReusableCell(withIdentifier: "ChordToneReusableCell")
+        
+        cell?.contentView.backgroundColor = UIColor.clear
+        cell?.backgroundColor = UIColor.clear
+        cell?.textLabel?.text = "1234"
+        
+        return cell!
+    }
+    
+    
+//----------------------------------------------------------------------------------------------------------
+// Popup Selectors controlls
+//----------------------------------------------------------------------------------------------------------
     @IBOutlet weak var chordButton: UIButton!
     
     @IBAction func chordPopUp(_ sender: UIButton) {
@@ -31,6 +55,10 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         }
     }
     
+    
+//----------------------------------------------------------------------------------------------------------
+// Overriden Functions
+//----------------------------------------------------------------------------------------------------------
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -41,6 +69,17 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "background.png")
         self.view.insertSubview(backgroundImage, at: 0)
+        
+        DroneTableView.dataSource = self
+        DroneTableView.backgroundColor = UIColor.clear
+        DroneTableView.alwaysBounceVertical = false;
+        
+        DispatchQueue.main.async {
+            //This code will run in the main thread:
+            var frame = self.DroneTableView.frame;
+            frame.size.height = self.DroneTableView.contentSize.height;
+            self.DroneTableView.frame = frame;
+            };
     }
     
     override func didReceiveMemoryWarning() {
